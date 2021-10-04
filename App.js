b4f70a8ab311ef2a7b8b13af76b70aa01c8bf651
Al Amin Kabbo <83044858+AlAminKabbo@users.noreply.github.com>
2021-10-04 12:28:40 +0600

    ud

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { colors } from './src/theme';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>hi Kabbo</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: colors.lightGreen,
     alignItems: 'center',
     justifyContent: 'center',
   },
