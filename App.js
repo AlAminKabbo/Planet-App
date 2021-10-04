a3623f677837ac5542fd8c4df28c349e389f977f
Al Amin Kabbo <83044858+AlAminKabbo@users.noreply.github.com>
2021-10-04 12:57:49 +0600

    ud

--- App.js
+++ App.js
@@ -2,11 +2,21 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { colors } from './src/theme';
+import { useFonts } from 'expo-font'
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    'Antonio-Medium': require('./assets/fonts/Antonio-Medium.ttf'),
+    'Spartan-Bold': require('./assets/fonts/Spartan-Bold.ttf'),
+    'Spartan-Regular': require('./assets/fonts/Spartan-Regular.ttf'),
+  });
+  if(!fontsLoaded){
+    return <Text>Loading...</Text>
+  }
+
   return (
     <View style={styles.container}>
-      <Text>hi Kabbo</Text>
+      <Text style={{fontFamily:'Spartan-Bold'}}>Hi Kabbo</Text>
       <StatusBar style="auto" />
     </View>
   );
