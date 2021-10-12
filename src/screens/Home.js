dca064e53ed8204c9455c8b8f6156db8063dacf2
Al Amin Kabbo <83044858+AlAminKabbo@users.noreply.github.com>
2021-10-12 18:08:34 +0600

    ud

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { View, Pressable, StatusBar, FlatList, StyleSheet, TouchableOpacity} from 'react-native';
+import React, {useState} from 'react';
+import { View, Pressable, StatusBar, FlatList, StyleSheet, TouchableOpacity, TextInput} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import  Text from '../components/text';
 import { colors, spacing } from '../theme';
@@ -18,7 +18,7 @@ export const PLANET_LIST = [
     wikiLink: "https://en.wikipedia.org/wiki/Mercury&quot",
     structure: "Mercury appears to have a solid silicate crust and mantle overlying a solid, iron sulfide outer core layer, a deeper liquid core layer, and a solid inner core. The planet's density is the second highest in the Solar System at 5.427 g/cm3 , only slightly less than Earth's density.",
     surface: "Mercury's surface is similar in appearance to that of the Moon, showing extensive mare-like plains and heavy cratering, indicating that it has been geologically inactive for billions of years. It is more heterogeneous than either Mars's or the Moon’s.",
-    surfaceImage: require('../../assets/geology-mercury.png')
+    surfaceImage: require('../../assets/1.png')
     },
     {
     name: 'venus',
@@ -31,7 +31,7 @@ export const PLANET_LIST = [
     wikiLink: "https://en.wikipedia.org/wiki/Venus&#39",
     structure: "The similarity in size and density between Venus and Earth suggests they share a similar internal structure: a core, mantle, and crust. Like that of Earth, Venusian core is most likely at least partially liquid because the two planets have been cooling at about the same rate.",
     surface: "Much of the Venusian surface appears to have been shaped by volcanic activity. Venus has several times as many volcanoes as Earth, and it has 167 large volcanoes that are over 100 km (60 mi) across. The only volcanic complex of this size on Earth is the Big Island of Hawaii.",
-    surfaceImage: require('../../assets/geology-venus.png')
+    surfaceImage: require('../../assets/2.png')
     },
     {
     name: 'earth',
@@ -44,7 +44,7 @@ export const PLANET_LIST = [
     wikiLink: "https://en.wikipedia.org/wiki/Earth&#39",
     structure: "Earth's interior, like that of the other terrestrial planets, is divided into layers by their chemical or physical (rheological) properties. The outer layer is a chemically distinct silicate solid crust, which is underlain by a highly viscous solid mantle.",
     surface: "The total surface area of Earth is about 510 million km2. The continental crust consists of lower density material such as the igneous rocks granite and andesite. Less common is basalt, a denser volcanic rock that is the primary constituent of the ocean floors.",
-    surfaceImage: require('../../assets/geology-earth.png')
+    surfaceImage: require('../../assets/3.png')
     },
     {
     name: 'mars',
@@ -57,7 +57,7 @@ export const PLANET_LIST = [
     wikiLink: 'https://en.wikipedia.org/wiki/Mars&#39',
     structure: "Like Earth, Mars has differentiated into a dense metallic core overlaid by less dense materials. Scientists initially determined that the core is at least partially liquid. Current models of its interior imply a core consisting primarily of iron and nickel with about 16–17% sulfur.",
     surface: "Mars is a terrestrial planet whose surface consists of minerals containing silicon and oxygen, metals, and other elements that typically make up rock. The surface is primarily composed of tholeiitic basalt, although parts are more silica-rich than typical basalt.",
-    surfaceImage: require('../../assets/geology-mars.png')
+    surfaceImage: require('../../assets/4.png')
     },
     {
     name: 'jupiter',
@@ -70,7 +70,7 @@ export const PLANET_LIST = [
     wikiLink: 'https://en.wikipedia.org/wiki/Jupiter&#39',
     structure: "When the Juno arrived in 2016, it found that Jupiter has a very diffuse core that mixes into its mantle. A possible cause is an impact from a planet of about ten Earth masses a few million years after Jupiter's formation, which would have disrupted an originally solid Jovian core.",
     surface: "The best known feature of Jupiter is the Great Red Spot, a persistent anticyclonic storm located 22° south of the equator. It is known to have existed since at least 1831, and possibly since 1665.",
-    surfaceImage: require('../../assets/geology-jupiter.png')
+    surfaceImage: require('../../assets/5.png')
     },
     {
     name: 'saturn',
@@ -83,7 +83,7 @@ export const PLANET_LIST = [
     wikiLink: 'https://en.wikipedia.org/wiki/Saturn&#39',
     structure: "Despite consisting mostly of hydrogen and helium, most of Saturn's mass is not in the gas phase, because hydrogen becomes a non-ideal liquid when the density is above 0.01 g/cm3, which is reached at a radius containing 99.9% of Saturn's mass.",
     surface: "The outer atmosphere of Saturn contains 96.3% molecular hydrogen and 3.25% helium by volume. The planet's most famous feature is its prominent ring system, which is composed mostly of ice particles with a smaller amount of rocky debris and dust. ",
-    surfaceImage: require('../../assets/geology-saturn.png')
+    surfaceImage: require('../../assets/6.png')
     },
     {
     name: 'uranus',
@@ -96,7 +96,7 @@ export const PLANET_LIST = [
     wikiLink: 'https://en.wikipedia.org/wiki/Uranus&#39',
     structure: "The standard model of Uranus's structure is that it consists of three layers: a rocky (silicate/iron–nickel) core in the centre, an icy mantle in the middle and an outer gaseous hydrogen/helium envelope. The core is relatively small, with a mass of only 0.55 Earth masses.",
     surface: "The composition of Uranus's atmosphere is different from its bulk, consisting mainly of molecular hydrogen and helium. The helium molar fraction, i.e. the number of helium atoms per molecule of gas, is 0.15±0.03 in the upper troposphere.",
-    surfaceImage: require('../../assets/geology-uranus.png')
+    surfaceImage: require('../../assets/7.png')
     },
     {
     name: 'neptune',
@@ -109,11 +109,12 @@ export const PLANET_LIST = [
     wikiLink: 'https://en.wikipedia.org/wiki/Neptune&#39',
     structure: "Neptune's internal structure resembles that of Uranus. Its atmosphere forms about 5% to 10% of its mass and extends perhaps 10% to 20% of the way towards the core. Increasing concentrations of methane, ammonia and water are found in the lower regions.",
     surface: "Neptune's atmosphere is 80% hydrogen and 19% helium. A trace amount of methane is also present. Prominent absorption bands of methane exist at wavelengths above 600 nm, in the red and infrared portion of the spectrum.",
-    surfaceImage: require('../../assets/geology-neptune.png')
+    surfaceImage: require('../../assets/8.png')
     },
     ];
   
 export default function HomeScreen({ navigation }) {
+    const [planetList, setPlanetList] = useState(PLANET_LIST)
     const renderItem = ({item,index}) => {
         const {name, color} = item
         return(
@@ -126,11 +127,29 @@ export default function HomeScreen({ navigation }) {
             </TouchableOpacity>
         )
     }
+    const searchFilter = (text) => {
+        const filterdList = PLANET_LIST.filter(item =>{
+            const itemData = item.name.toLowerCase()
+            const  userTypedText = text.toLowerCase()
+
+            return itemData.indexOf(userTypedText) > -1
+        })
+        setPlanetList(filterdList)
+    }
     return (
         <View style={styles.body}>
             <PlanetHeader/>
+            <View style={styles.box}>
+            <TextInput
+                placeholder = "Search planet by name"
+                placeholderTextColor = {colors.white}
+                onChangeText={(text)=>{searchFilter}}
+                autoCorrect = {false}
+                style = {styles.searchBox}
+            />
+            </View>
             <FlatList
-                data={PLANET_LIST}
+                data={planetList}
                 renderItem={renderItem}
                 keyExtractor={(item,index) => item.name}
                 contentContainerStyle={{ padding: spacing[3]}}
@@ -165,5 +184,16 @@ const styles = StyleSheet.create({
     planet:{
         flexDirection: 'row', 
         alignItems: 'center',
+    },
+    searchBox:{
+        padding: spacing[1],
+        borderColor: colors.white,
+        borderWidth: 1,
+        borderRadius: 12,
+        color: colors.white,
+    },
+    box:{
+     marginHorizontal: spacing[2],
+     marginTop: spacing[2],
     }
 })
